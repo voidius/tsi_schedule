32d0ac472d6758e8283426af7cad36682d5dd3d9
Andrew Trabo <v01d3r@gmail.com>
2012-12-24 22:50:43 +0200

    Remove 'test' directory

--- test/functional/users/omniauth_callbacks_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class Users::OmniauthCallbacksControllerTest < ActionController::TestCase
-  test "should get facebook" do
-    get :facebook
-    assert_response :success
-  end
-
-end
